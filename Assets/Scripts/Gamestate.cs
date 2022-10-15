1a8e7a0644cd9d1f066f9edabbb155542e1fb7ba
DESKTOP-DL6MO1K\Zeeshan Qaswar <xeeshanqaswar@gmail.com>
2022-10-15 18:40:38 +0500

    #FEATURES Working on Selection Screen and Gameplay system.

---
+++ Assets/Scripts/Gamestate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Gamestate", menuName = "CustomObjects/Gamestate")]
+public class Gamestate : ScriptableObject
+{
+    public GameMode selectedGameMode;
+    public Region selectedRegion;
+}
+
+[Serializable]public enum GameMode { FreeRoaming, StoryMode }
+[Serializable] public enum Region { Hunza, Muree, Skardu }
---
+++ Assets/Scripts/Selection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Selection : MonoBehaviour
+{
+
+    #region FIELDS DECLERATION
+
+    public Gamestate gamestate;
+
+    #endregion
+
+    public void SelectRegion(int state)
+    {
+        gamestate.selectedRegion = (Region)state;
+    }
+
+    public void SelectGamemode(int index)
+    {
+        gamestate.selectedGameMode = (GameMode)index;
+    }
+
+}
