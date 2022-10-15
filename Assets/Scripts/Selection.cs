d08e4fdeeb7130394ac42a849f2eeb829553a62a
DESKTOP-DL6MO1K\Zeeshan Qaswar <xeeshanqaswar@gmail.com>
2022-10-15 22:52:07 +0500

    #FEATURES Selection Scene Vehicle Selection Scene.

--- Assets/Scripts/Selection.cs
+++ Assets/Scripts/Selection.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 
 public class Selection : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class Selection : MonoBehaviour
     #region FIELDS DECLERATION
 
     public Gamestate gamestate;
+    
 
     #endregion
 
@@ -21,5 +23,11 @@ public class Selection : MonoBehaviour
     {
         gamestate.selectedGameMode = (GameMode)index;
     }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
 
 }
