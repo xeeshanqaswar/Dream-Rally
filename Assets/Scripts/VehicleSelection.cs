e067adbee909f4450de7977542bb2d283a587081
DESKTOP-DL6MO1K\Zeeshan Qaswar <xeeshanqaswar@gmail.com>
2022-10-16 19:41:22 +0500

    #FEATURE Vehicle Selection Animatins

---
+++ Assets/Scripts/VehicleSelection.cs
@@ -0,0 +1,44 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VehicleSelection : MonoBehaviour
+{
+    [Header("UI REFERENCES")]
+    public TextMeshProUGUI displayName, displayPrice;
+    public Image speed, acceleration, handling;
+
+    private int currentVehicleIndex = 0;
+
+    private void OnEnable()
+    {
+        StartCoroutine(UpdateStats());
+    }
+
+    IEnumerator UpdateStats()
+    {
+        // Reset
+        displayName.rectTransform.DOAnchorPos(new Vector2(-700f, -70f), 0f);
+        displayPrice.rectTransform.DOAnchorPos(new Vector2(-700f, -175f), 0f);
+
+        speed.fillAmount = 0f;
+        acceleration.fillAmount = 0f;
+        handling.fillAmount = 0f;
+
+        yield return new WaitForSeconds(0.1f);
+        
+        // Animation
+        displayName.rectTransform.DOAnchorPos(new Vector2(80f, -70f), 0.5f);
+        displayPrice.rectTransform.DOAnchorPos(new Vector2(80f, -175f), 0.5f).SetDelay(0.1f);
+
+        speed.DOFillAmount(0.5f, 0.5f); 
+
+        acceleration.DOFillAmount(0.5f, 0.5f);
+        
+        handling.DOFillAmount(0.5f, 0.5f);
+    }
+
+}
